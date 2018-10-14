c0794ca0fd28164ec11541f324478c8aa4027191
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-10-14 14:33:27 -0300

    Criação rota /oi

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ express.set('view engine', 'ejs');
 express.set('views', './views');
 
 express.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {oi:{}});
+});
+
+express.get('/oi', (req, res) => {
+  res.render('index',{oi:true});
 });
 
 express.listen(3000, () =>{
